cde82cdb4c45dbc5aafbea3488a1ef170158b331
vsdeshinge <maharshivivekanands@gmail.com>
2024-05-05 11:24:44 +0530

    frontend

--- server2/app.js
+++ server2/app.js
@@ -24,38 +24,56 @@ app.use(express.static(`${__dirname}/public`));
 //middle ware
 
 
-const Product = require('./models/productModel');
+const product = require('./models/productModel');
 const APIFeatures = require('./utils/apiFeatures');
 
-// app.get('/', async (req, res) => {
-//   try {
 
-//     const features = new APIFeatures(Product.find(), req.query)
-//             .filter()
-//             .sort()
-//             .limitFields()
-//             .paginate();
+app.get('/', async (req, res) => {
+  try {
 
-//         const products = await features.query;
+    // const features = new APIFeatures(Product.find(), req.query)
+    //         .filter()
+    //         .sort()
+    //         .limitFields()
+    //         .paginate();
 
-//     res.status(200).json({
-//       status: 'success',
-//       requestedAt: req.requestTime,
-//       data: products
-//     });
-//   } catch (err) {
-//     console.error('Error fetching data:', err);
-//     res.status(500).json({ status: 'error', message: 'Error retrieving data' });
-//   }
-// });
+        const products = await product.find();
+
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      data: products
+    });
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    res.status(500).json({ status: 'error', message: 'Error retrieving data' });
+  }
+});
+
+app.get('/:id',async (req,res)=>{
+    try{
+        const product = await product.findById(req.params.id)
+        res.status(200).json({
+            status: 'success',
+            data:{
+                product
+            }
+            
+        });
+    
+        }catch(err){
+            res.send(404).json({
+                status:'fail',
+                message:'invalid id'
+            })
+        }
+    });    
 
 
-// app.post('/',(req, res, data)=>{
-
-// })
 
 
-app.use('/',productRouter)
+
+
 
 
 
