165e8ee10fb715ab0165943b5a90fba8c808fb39
vsdeshinge <maharshivivekanands@gmail.com>
2024-05-10 23:37:33 +0530

    loginSignUpdone

--- server/controllers/loginSignUpController.js
+++ server/controllers/loginSignUpController.js
@@ -1,46 +1,48 @@
 
 const User = require('./../models/signUpModel');
 const validator = require('validator');
+const catchAsynch = require('./../utils/catchAsync')
 
 
 
-exports.getlogin = async(req, res, next) => {
-    console.log(req.body);
-    next()
-    const usernameOrEmail = req.body.name;
-    
-
-    const password = req.body.password;
-    let data = "invalid username/password "
-
-    
-
-
-    await User
-      .find({ $or: [
-         { username: usernameOrEmail },
-         { email: usernameOrEmail }
-       ] }, { username: 1,email:1, password: 1, _id: 0 })
-      .then(docs => {
-        console.log('Documents retrieved:', docs);
-        if (docs.length > 0) {
-          const singleObject = docs[0];
-          if (password === singleObject.password) {
-            data ="Authentication successful"
+exports.getlogin = catchAsynch(
+   
+   async(req, res, next) => {
+       console.log(req.body);
+       next()
+       const usernameOrEmail = req.body.name;
+       
+   
+       const password = req.body.password;
+       let data = "invalid username/password "
+   
+       
+   
+   
+       await User
+         .find({ $or: [
+            { username: usernameOrEmail },
+            { email: usernameOrEmail }
+          ] }, { username: 1,email:1, password: 1, _id: 0 })
+         .then(docs => {
+           console.log('Documents retrieved:', docs);
+           if (docs.length > 0) {
+             const singleObject = docs[0];
+             if (password === singleObject.password) {
+               data ="Authentication successful"
+              
+               
+             }
+           }
+   
+           console.log(data);
            
-            
-          }
-        }
+         })
+      }
+);
+      
 
-        console.log(data);
-        
-      })
-      .catch(err => {
-        console.error('Error retrieving documents', err);
-        res.status(500).send('Internal Server Error');
-      })
 
-};
         
         
         
@@ -52,7 +54,7 @@ exports.getlogin = async(req, res, next) => {
 
 
 
- exports.getSignUp = async(req, res, next)=>{
+ exports.getSignUp = catchAsynch(async(req, res, next)=>{
 
    const newUser= await User.create(req.body);
         res.status(201).json({
@@ -62,4 +64,4 @@ exports.getlogin = async(req, res, next) => {
             }
         });
     //next();
- }
+ });
