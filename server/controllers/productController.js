7f665385da691c8bac7b05b6c3086debdbe6d4e4
vsdeshinge <maharshivivekanands@gmail.com>
2024-05-11 12:45:12 +0530

    added catchAsych and error

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('./../models/productModel')
 const APIFeatures = require('./../utils/apiFeatures')
 const catchAsync = require('./../utils/catchAsync')
+const AppError = require('./../utils/appError')
 
 
 exports.getAllProducts = catchAsync(async (req, res, next) =>{
@@ -15,6 +16,10 @@ exports.getAllProducts = catchAsync(async (req, res, next) =>{
             .paginate();
 
         const products = await features.query;
+        if (product.length === 0) {
+            return next(new AppError(`No Product find`, 404));
+
+        }
         
         
         res.status(200).json({
@@ -32,13 +37,13 @@ exports.getAllProducts = catchAsync(async (req, res, next) =>{
 exports.getProduct = catchAsync(async(req, res, next) =>{
     
     
-        const id = req.params.id*1;
+    const id = req.params.id;
         
     const product = await Product.find({id: id});
          //Tour.findOne({_id:req.param.id}) both are similar way
 
         if (product.length === 0) {
-            throw new Error('Product not found');
+            return next(new AppError(`No Product find`, 404));
     }
   
     res.status(200).json({
@@ -66,7 +71,11 @@ exports.getProduct = catchAsync(async(req, res, next) =>{
 
 exports.createProduct= catchAsync(async(req, res, next)=>{
     
-        const newProduct= await Product.create(req.body);
+        const product= await Product.create(req.body);
+        if (product.length === 0) {
+            return next(new AppError(`No Product find`, 404));
+
+        }
         res.status(201).json({
             status: 'success',
             data:{
@@ -87,6 +96,11 @@ exports.updateProduct= catchAsync(async(req,res, next )=>{
             new : true,
             runValidators: true
         })
+
+        if (product.length === 0) {
+            return next(new AppError(`No Product find`, 404));
+
+        }
         res.status(200).json({
             status: 'success',
             data:{
